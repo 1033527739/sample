2294ebdf461af4120e775ad5cd626000de5955e8
Christian Cheng <1033527739@qq.com>
2017-08-12 15:54:35 +0800

    Move user model to models folder

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model'  => App\User::class,
+        'model'  => APP\Models\Users::class,
         'key'    => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
